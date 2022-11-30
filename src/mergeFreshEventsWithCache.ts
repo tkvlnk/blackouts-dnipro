1b77c88f1fc61966dd8b680f09e4eb63f635fab6
Tit Kovalenko <titk@wix.com>
2022-11-30 21:40:30 +0200

    sort cached events

--- src/mergeFreshEventsWithCache.ts
+++ src/mergeFreshEventsWithCache.ts
@@ -6,11 +6,20 @@ export function mergeFreshEventsWithCache(cachedEvents: EventAttributes[], fresh
   return [
     ...cachedEvents.filter(event => eventStartDiffInMs(event) < 0),
     ...freshEvents.filter(event => eventStartDiffInMs(event) > 0)
-  ];
+  ].sort((leftEvent, rightEvent) => {
+    const leftMs = eventToStartDateTime(leftEvent).millisecond;
+    const rightMs = eventToStartDateTime(rightEvent).millisecond;
+
+    if (leftMs === rightMs) {
+      return 0;
+    }
+
+    return leftMs > rightMs ? 1 : -1
+  });
 }
 
-function eventStartDiffInMs({start: [year, month, day, hour, minute]}: EventAttributes) {
-  const eventDateTime = DateTime.fromObject({
+function eventToStartDateTime({start: [year, month, day, hour, minute]}: EventAttributes): DateTime {
+  return DateTime.fromObject({
     year,
     month,
     day,
@@ -18,7 +27,11 @@ function eventStartDiffInMs({start: [year, month, day, hour, minute]}: EventAttr
     minute,
   }, {
     zone: UA_TIMEZONE
-  });
+  })
+}
+
+function eventStartDiffInMs(event: EventAttributes) {
+  const eventDateTime = eventToStartDateTime(event);
 
   const {milliseconds} = eventDateTime.diffNow('milliseconds');
 
