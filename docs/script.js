8c188d81a7a4ec4ed274a4af1aee9db6fbee4b90
Tit Kovalenko <titk@wix.com>
2022-12-01 10:22:02 +0200

    improve html

--- docs/script.js
+++ docs/script.js
@@ -3,11 +3,34 @@
   '2',
   '3'
 ].forEach((id) => {
-  const btn = document.getElementById(`group-${id}`);
+  const wrap = document.getElementById(`group-${id}`);
 
-  btn.textContent = `Група ${id} - Koпіювати посилання`;
+  wrap.append(
+    createAnchor(id),
+    createSpanSeparator(),
+    createButton(id)
+  )
+})
 
+function createSpanSeparator() {
+  const elem = document.createElement('span');
+  elem.innerHTML = `&nbsp;`
+  return elem;
+}
+
+function createButton(id) {
+  const btn = document.createElement('button');
+  btn.textContent = `Koпіювати посилання`;
   btn.addEventListener('click', () => {
     navigator.clipboard.writeText(`${location.origin}${location.pathname}group-${id}.ics`)
   })
-})
+  return btn;
+}
+
+function createAnchor(id) {
+  const anch = document.createElement('a');
+  anch.textContent = `Група ${id}`;
+  anch.href = `webcal://${location.hostname}${location.pathname}group-${id}.ics`;
+  return anch;
+}
+
