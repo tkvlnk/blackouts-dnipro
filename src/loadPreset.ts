5b12c145015338397e72e06208b19a4abb86e90a
Tit Kovalenko <titk@wix.com>
2023-02-01 23:10:03 +0200

    fix loading preset

--- src/loadPreset.ts
+++ src/loadPreset.ts
@@ -1,9 +1,17 @@
-import puppeteer from "puppeteer";
+import puppeteer, {Page, TimeoutError} from "puppeteer";
 
 export interface SchedulePreset {
   sch_names: Record<string, string>;
   time_zone: Record<string, string>;
   data: Record<string, Record<string, number[]>>;
+}
+
+declare global {
+  interface Window {
+    DisconSchedule: {
+      preset: SchedulePreset
+    }
+  }
 }
 
 export async function loadPreset(): Promise<SchedulePreset> {
@@ -14,11 +22,42 @@ export async function loadPreset(): Promise<SchedulePreset> {
     waitUntil: "networkidle2",
   });
 
-  // @ts-expect-error
-  const schedule = await page.evaluate(() => DisconSchedule)
+  const preset = await waitFor(page, () => extractPreset(page));
 
   await browser.close();
 
-  return schedule.preset;
+  return preset;
 }
 
+function waitFor<V>(page: Page, tryGetValue: () => Promise<V | null>, {timeout = 60_000} = {}): Promise<V> {
+  return new Promise<V>(async (resolve, reject) => {
+    const startTs = Date.now();
+
+    let value: V | null;
+
+    do {
+      value = await tryGetValue();
+      await new Promise(res => setTimeout(res, 5000));
+      console.log('>__value:::', value)
+    } while (value == null && (Date.now() - startTs < timeout));
+
+    if (value) {
+      resolve(value);
+    } else {
+      reject(new Error(`Timeout of ${timeout} ms was exceeded`));
+    }
+  })
+}
+
+function extractPreset(page: Page) {
+  return page.evaluate(
+    () => {
+      var myScript = document.createElement("script");
+      myScript.innerHTML = `
+    window.DisconSchedule = DisconSchedule`;
+      document.body.appendChild(myScript);
+
+      return window.DisconSchedule.preset
+    }
+  ).catch(() => null)
+}
